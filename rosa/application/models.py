c78f2791788d2949667979ed6aca390210b27f2d
Chris Shenton <chris@koansys.com>
2012-03-22 12:01:45 -0400

    Skeleton for 'apps'

---
+++ rosa/application/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+# Create your models here.
+
+class Application(Model):
+    acronym = CharField(max_length=128, label="Acronym");
+# Acronym
+# Version Number
+# Name
+# Description
+# Support Class
+# NASA Owner Name
+# Application Type
+# NASA Analyst Name
+# Software Class
+# User Groups
+# Frequency Used
+# Number of Users
+# Version Status
+# Disposition Comments
+# Version Change Description
+# Release Date
+# Release Notes
+# Service Request Numbers
+# Service Request Class(s)
+# HITSS Supported
+# NASA Owner Organization Name
+# NASA Owner Office ID
+# NASA Requestor
+# Contract Task Order Numbers
+# ODIN Triage Level
+# Branch Manager Name
+# Project Manager Name
+# Developer - Primary
+# Developer - Alternate
+# GOTS Agency Contact Information
+# Functional Types
+# Data Impact Type
+# Information Sensitivity
+# FIPS Information Category
+# Privacy Act System
+# Privacy Info Indicator
+# Privacy Info Data Types
+# Compliance - 2810
+# Compliance - 508
+# Compliance - AWRS
+# AWRS Check List on File
+# IT&CD Security Owner Name
+# Authentication Type
+# Firewall w/2 Factor Token Authentication
+# Contractor/Civil Servant Managed Records
+# SSN System
+# Filed with Federal Registry
+# Internal or External System
+# SORN - System of Records Notice Number
+# Security Plan Number
+# URL Link
+# Architecture Type
+# DBMS Names and Version
+# Software Names and Versions
+# Web Browsers
+# Servers - Application
+# Servers - Database
+# Servers - Report
+# Servers Location
+# Network Services Used
+# Interface Acronym
+# Interface Direction
+# Interface Method
+# Federal Records Qualification
+# NRRS Disposition
+# NRRS Schedule/Item
+
