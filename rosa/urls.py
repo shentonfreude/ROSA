d5ee8e86f840657f53af60bb41a084ec8ead0909
Chris Shenton <chris@koansys.com>
2012-04-01 08:54:10 -0400

    Search from home page, colorize versions, results by app_status (M2M!)

--- rosa/urls.py
+++ rosa/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
+from application.views import home
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$',     direct_to_template, {'template': 'home.html'}, name='home'),
+    url(r'^$',       home, name='home'),
     # url(r'^help$', direct_to_template, {'template': 'help.html'}, name='help'),
 
     url(r'^application/', include('application.urls')),
