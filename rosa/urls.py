1f79700df5c7a73c3883b99da68ac288c8c0684f
Chris Shenton <chris@koansys.com>
2012-04-02 18:51:51 -0400

    Closes #15 emit "[No#]" in templates for empty ('') release attr

--- rosa/urls.py
+++ rosa/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 #from application.views import home
 from views import home, about
 
@@ -18,6 +18,10 @@ urlpatterns = patterns(
 
     # Uncomment the admin/doc line below to enable admin documentation:
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # This is a hack to make the menu bar cog link work.
+    # django/contrib/admin/sites.get_urls says ^$ is 'index' -- how to use?
+    url(r'^admin$', redirect_to, {'url': '/admin/'}, name='manage'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
